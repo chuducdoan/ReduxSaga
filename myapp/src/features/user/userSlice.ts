01a3006a2a65f92ae5b32edf43c3c41d1e997d95
DoanCD <chuducdoan3011@gmail.com>
2023-06-30 17:44:23 +0700

    create user slice from react toolkit

---
+++ myapp/src/features/user/userSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = null;
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
